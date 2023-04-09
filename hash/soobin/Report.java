be0062096a69b70925e60860d6bc0e6c1b8734f6
s0o0bn <0921soobin@gmail.com>
2023-04-09 14:12:59 +0900

    FIX 프로그래머스 92334 속도 최적화

--- hash/soobin/Report.java
+++ hash/soobin/Report.java
@@ -7,35 +7,34 @@ public class Report {
     public static List<Integer> solution(String[] id_list, String[] report, int k) {
         HashMap<String, List<String>> who = new HashMap<>();
         HashMap<String, Integer> reported = new HashMap<>();
+        List<String> filteredReport = Arrays.stream(report).distinct().collect(Collectors.toList());
         List<Integer> answer = new ArrayList<>();
 
-        for(String reportInfo : report) {
+        for(String reportInfo : filteredReport) {
             StringTokenizer st = new StringTokenizer(reportInfo);
             String from = st.nextToken();
             String to = st.nextToken();
 
-            if(who.get(from) != null && who.get(from).stream().anyMatch(to::equals)) continue;
             reported.put(to, reported.getOrDefault(to, 0) + 1);
             who.computeIfAbsent(from, x -> new ArrayList<>()).add(to);
         }
 
-        List<String> blocked = reported.entrySet().stream()
-                .filter(entry -> entry.getValue() >= k)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
-                .keySet().stream().collect(Collectors.toList());
-        Arrays.stream(id_list).forEach((id)-> answer.add(getReportMail(who.get(id), blocked)));
+        HashSet<String> blocked = new HashSet<>();
+        reported.entrySet().forEach(entry -> {
+            if(entry.getValue() >= k) blocked.add(entry.getKey());
+        });
+        Arrays.stream(id_list).forEach(id -> {
+            List<String> id_report = who.get(id);
+            int count = id_report == null ? 0 : (int) id_report.stream().filter(i -> blocked.contains(i)).count();
+            answer.add(count);
+        });
 
         return answer;
     }
 
     public static void main(String[] args) {
-        String[] id_list = {"con", "ryan", "apeach"};
-        String[] report = {"con apeach", "ryan apeach", "con ryan", "ryan apeach", "apeach con", "apeach con"};
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
         System.out.println(solution(id_list, report, 2));
-    }
-
-    private static int getReportMail(List<String> reported, List<String> blocked) {
-        return reported == null ? 0
-                : reported.stream().flatMap(i -> blocked.stream().filter(j -> i.equals(j))).toArray().length;
     }
 }
