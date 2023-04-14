00c53dd2441c0afe8ce96f601460d25f2038d1b7
yoona <yoona0427@naver.com>
2023-04-15 02:42:14 +0900

    프로그래머스 42577 성공

--- hash/yoona/전화번호_목록.py
+++ hash/yoona/전화번호_목록.py
@@ -1,22 +1,13 @@
-from collections import defaultdict
 def solution(phone_book):
     answer = True
     
-    phone_book.sort(key=lambda x : len(x)) # 길이순으로 정렬
+    phone_book = sorted(phone_book)
     
-    hash_table = defaultdict(int)
-    for phone_number in phone_book:
-        if not hash_table:
-            hash_table[phone_number] += 1 # 첫 원소(제일 짧은)는 무조건 추가
-        
+    temp = phone_book[0]
+    for index in range(1, len(phone_book)):
+        if phone_book[index].startswith(temp):
+            return False
         else:
-            heads = hash_table.keys()
-            print(heads)
-            for head in heads:
-                print(head)
-                if phone_number.startswith(head):
-                    return False
-            
+            temp = phone_book[index]
+        
     return answer
-
-solution(["119", "97674223", "1195524421"])
