24851980c6f905bdb779c05d33a00e2777edc42e
김정례 <thuncol12@naver.com>
2023-04-20 22:48:30 +0900

    문자열폭발 성공

--- stack/jungrye/025_문자열폭발.py
+++ stack/jungrye/025_문자열폭발.py
@@ -1,15 +1,21 @@
-# 스택안씀, 시간초과
 import sys
 
 str = sys.stdin.readline().strip()
 boom = sys.stdin.readline().strip()
+reversedBoom = "".join(reversed(boom))
+stack = []
 
-while True:
-    if len(str) == 0:
-        print("FRULA")
-        break
-    if boom in str:
-        str = str.replace(boom, '')
+for s in str:
+    if s == boom[-1]:  # 폭발 문자열의 마지막 문자와 같은지 검사
+        stack.append(s)
+        isBoom = ''.join(stack[-1:-len(boom)-1:-1])
+        if isBoom == reversedBoom:
+            for i in range(len(boom)):
+                stack.pop()
     else:
-        print(str)
-        break
+        stack.append(s)
+
+if len(stack) == 0:
+    print("FRULA")
+else:
+    print(''.join(stack))
