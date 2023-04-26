e797c7c592cc5d0c7fbbab42afb21a51e082681f
yoona <yoona0427@naver.com>
2023-04-26 21:30:45 +0900

    프로그래머스 42587 코드 수정

--- queue/yoona/프로세스.py
+++ queue/yoona/프로세스.py
@@ -2,18 +2,19 @@ from collections import deque
 
 
 def solution(priorities, location):
-    sorted_priorities = sorted(priorities, reverse=True)
     priorities = deque([(i, p) for i, p in enumerate(priorities)])
+
     cnt = 0
-    for priority in sorted_priorities:
-        while priorities:
-            index, value = priorities.popleft()
+    while priorities:
+        max_value = max(priorities, key=lambda x: x[1])[1]
+        index, value = priorities.popleft()
 
-            if value < priority:
-                priorities.append((index, value))
+        if value < max_value:
+            priorities.append((index, value))
 
-            elif value == priority:
-                cnt += 1
-                if location == index:
-                    return cnt
+        elif value == max_value:
+            cnt += 1
+            if location == index:
                 break
+
+    return cnt
