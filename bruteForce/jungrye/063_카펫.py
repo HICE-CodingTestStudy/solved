107c9f5148609f456a51d5f6bb2aa1d1b1d23507
김정례 <thuncol12@naver.com>
2023-05-17 01:48:21 +0900

    카펫 완

--- bruteForce/jungrye/063_카펫.py
+++ bruteForce/jungrye/063_카펫.py
@@ -1,16 +1,10 @@
 def solution(brown, yellow):
     answer = []
     mult = brown+yellow
-    # print("mult", mult)
     for i in range(3, mult):
         if mult % i == 0:
             j = mult//i
-            # print('후보')
-            # print("i", i)
-            # print("j", j)
             if (2*i+(j*2-4)) == brown or ((i*2-4)+2*j) == brown:
-                # print("아이", i)
-                # print('제이', j)
                 answer = [i, j]
                 answer.sort(reverse=True)
 
