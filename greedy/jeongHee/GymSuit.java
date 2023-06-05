28b9fdf3f90547938dff397bc9bfa572da3bd3c5
sjhlko <sjhlko@naver.com>
2023-06-05 10:59:40 +0900

    style: 그리디 과제(하) 체육복 주석 추가

--- greedy/jeongHee/GymSuit.java
+++ greedy/jeongHee/GymSuit.java
@@ -3,6 +3,9 @@ package greedy;
 import java.util.Arrays;
 
 public class GymSuit {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/42862
+    // 체육복
+    // 근데 5번이 없어서 1번이 2개 있을때 1번이 2번주고 2번이 3번주고 해서 하는건 안됨?
     public int solution(int n, int[] lost, int[] reserve) {
         int[] suit = new int[n];
         Arrays.fill(suit,1);
