28cbb478b63e2f9bae33a30643e42d97df5dd13b
sjhlko <sjhlko@naver.com>
2023-07-10 02:05:09 +0900

    feat: sql 연습문제(SELECT) 여러 기준으로 정렬하기

--- sql/jeongHee/Select.java
+++ sql/jeongHee/Select.java
@@ -35,3 +35,9 @@ where INTAKE_CONDITION != "Aged";
 SELECT ANIMAL_ID, NAME
 from ANIMAL_INS
 order by ANIMAL_ID asc;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/59404
+//여러 기준으로 정렬하기
+SELECT ANIMAL_ID, NAME, DATETIME
+from ANIMAL_INS
+order by NAME asc, DATETIME desc;
