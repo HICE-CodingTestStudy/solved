16673ad653c6d8ef45fc34f009d3841e902f7e99
sjhlko <sjhlko@naver.com>
2023-07-10 06:12:37 +0900

    feat: sql 연습문제(group by) 식품 분류별 가장 비싼 식품의 정보 조회하기

--- sql/jeongHee/GroupBy.java
+++ sql/jeongHee/GroupBy.java
@@ -12,3 +12,25 @@ SELECT b.INGREDIENT_TYPE, sum(a.TOTAL_ORDER) as TOTAL_ORDER
 from FIRST_HALF a inner join ICECREAM_INFO b on a.FLAVOR = b.FLAVOR
 group by b.INGREDIENT_TYPE
 order by sum(a.TOTAL_ORDER)
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/131116
+//식품 분류 별 가장 비싼 식품의 정보 조회하기
+
+//오답
+SELECT CATEGORY, max(PRICE) as MAX_PRICE, PRODUCT_NAME
+from FOOD_PRODUCT
+where CATEGORY in ("식용유","과자", "국", "김치")
+group by CATEGORY
+order by MAX_PRICE desc;
+//이 경우 최대값은 제대로 나오지만 해당 값에 대한 이름이 나오는게 아니라 그냥 냅다 처음게 나오는듯
+
+//정답
+SELECT CATEGORY,PRICE as MAX_PRICE, PRODUCT_NAME
+from FOOD_PRODUCT
+where PRICE
+in (select max(PRICE) as MAX_PRICE
+from FOOD_PRODUCT
+group by CATEGORY) and
+CATEGORY in ("식용유","과자", "국", "김치")
+group by CATEGORY
+order by MAX_PRICE desc;
