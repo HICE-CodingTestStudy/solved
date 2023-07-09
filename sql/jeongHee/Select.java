268311c63c7d5724b919d086f614e51414957056
sjhlko <sjhlko@naver.com>
2023-07-10 01:10:53 +0900

    feat: sql 연습문제(SELECT) 강원도에 위치한 생산공장 목록 출력하기

---
+++ sql/jeongHee/Select.java
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/131112
+// 강원도에 위치한 생산공장 목록 출력하기
+
+SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+        from FOOD_FACTORY
+        where ADDRESS like "강원%"
+        order by FACTORY_ID asc;
