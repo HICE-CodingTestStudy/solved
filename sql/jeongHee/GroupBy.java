b59dbc941685ee8f32b133957d2116de67152e53
sjhlko <sjhlko@naver.com>
2023-07-10 06:25:40 +0900

    feat: sql 연습문제(group by) 자동차 종류 별 특정옵션이 포함된 자동차 수 구하기

--- sql/jeongHee/GroupBy.java
+++ sql/jeongHee/GroupBy.java
@@ -34,3 +34,11 @@ group by CATEGORY) and
 CATEGORY in ("식용유","과자", "국", "김치")
 group by CATEGORY
 order by MAX_PRICE desc;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/151137
+//자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기
+SELECT CAR_TYPE, count(CAR_ID) as CARS
+from CAR_RENTAL_COMPANY_CAR
+where OPTIONS like "%시트%"
+group by CAR_TYPE
+order by CAR_TYPE
