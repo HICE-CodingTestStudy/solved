045a1237cb1e268dd66abb0eedbe1df00448e9ca
sjhlko <sjhlko@naver.com>
2023-07-10 05:54:01 +0900

    feat: sql 연습문제(group by) 성분으로 구분한 아이스크림 총 주문량

--- sql/jeongHee/GroupBy.java
+++ sql/jeongHee/GroupBy.java
@@ -5,3 +5,10 @@ from BOOK b inner join BOOK_SALES s on b.BOOK_ID=s.BOOK_ID inner join AUTHOR a o
 where s.SALES_DATE like "2022-01%"
 group by a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY
 order by a.AUTHOR_ID, b.CATEGORY desc;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/133026
+//성분으로 구분한 아이스크림 총 주문량
+SELECT b.INGREDIENT_TYPE, sum(a.TOTAL_ORDER) as TOTAL_ORDER
+from FIRST_HALF a inner join ICECREAM_INFO b on a.FLAVOR = b.FLAVOR
+group by b.INGREDIENT_TYPE
+order by sum(a.TOTAL_ORDER)
