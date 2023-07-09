e8a39a76b197ed5cfee792709e9a8c2f8f02c9cd
sjhlko <sjhlko@naver.com>
2023-07-10 02:54:16 +0900

    feat: sql 연습문제(SELECT) 3월에 태어난 여성 회원 목록 출력하기

--- sql/jeongHee/Select.java
+++ sql/jeongHee/Select.java
@@ -61,3 +61,10 @@ SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD,"%Y-%m-%d")
 from DOCTOR
 where MCDP_CD = "CS" or MCDP_CD = "GS"
 order by HIRE_YMD desc, DR_NAME asc;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/131120
+//3월에 태어난 여성 회원 목록 출력하기
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH,"%Y-%m-%d") as DATE_OF_BIRTH
+from MEMBER_PROFILE
+where TLNO != "NULL" and GENDER = 'W' and DATE_OF_BIRTH like "%-03-%"
+order by MEMBER_ID;
