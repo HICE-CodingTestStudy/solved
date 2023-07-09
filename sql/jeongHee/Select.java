cb8c16e90ed30971891811e0ed95a0f4694b2c27
sjhlko <sjhlko@naver.com>
2023-07-10 01:25:00 +0900

    feat: sql 연습문제(SELECT) 역순 정렬하기

--- sql/jeongHee/Select.java
+++ sql/jeongHee/Select.java
@@ -12,3 +12,8 @@ SELECT *
 from ANIMAL_INS
 order by ANIMAL_ID asc
 
+//https://school.programmers.co.kr/learn/courses/30/lessons/59035
+//역순 정렬하기
+SELECT NAME, DATETIME
+from ANIMAL_INS
+order by ANIMAL_ID desc;
