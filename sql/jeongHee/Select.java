21f7115613e81ae85c4c52691b2103910b8e3a82
sjhlko <sjhlko@naver.com>
2023-07-10 04:03:48 +0900

    feat: sql 연습문제(SELECT) 재구매가 일어난 상품과 회원 리스트 구하기

--- sql/jeongHee/Select.java
+++ sql/jeongHee/Select.java
@@ -116,3 +116,10 @@ order by AGE desc, PT_NAME;
 SELECT FLAVOR
 from FIRST_HALF
 order by TOTAL_ORDER desc, SHIPMENT_ID;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/131536
+//재구매가 일어난 상품과 회원 리스트 구하기
+SELECT USER_ID, PRODUCT_ID
+from ONLINE_SALE
+group by USER_ID,PRODUCT_ID having count(ONLINE_SALE_ID)>=2
+order by USER_ID, PRODUCT_ID desc;
