230ad9a1c3d2fb0f3bb50d45316b1fa6c5268196
sjhlko <sjhlko@naver.com>
2023-07-10 11:41:12 +0900

    feat: sql 연습문제(group by) 진료과별 총 예약 횟수 출력하기

--- sql/jeongHee/GroupBy.java
+++ sql/jeongHee/GroupBy.java
@@ -71,3 +71,11 @@ from BOOK b inner join BOOK_SALES s on b.BOOK_ID = s.BOOK_ID
 where s.SALES_DATE like "2022-01%"
 group by b.CATEGORY
 order by b.CATEGORY
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/132202
+//진료과별 총 예약 횟수 출력하기
+SELECT MCDP_CD as 진료과코드, count(APNT_YMD) as 5월예약건수
+from APPOINTMENT
+where APNT_YMD like "2022-05%"
+group by MCDP_CD
+order by 5월예약건수, 진료과코드;
