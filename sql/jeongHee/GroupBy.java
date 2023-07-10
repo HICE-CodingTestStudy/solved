3c411a4990691e0de45aa0a966555871b5c864fb
sjhlko <sjhlko@naver.com>
2023-07-10 11:32:11 +0900

    feat: sql 연습문제(group by) 조건에 맞는 사용자와 총 거래금액 조회하기

--- sql/jeongHee/GroupBy.java
+++ sql/jeongHee/GroupBy.java
@@ -54,3 +54,12 @@ group by FOOD_TYPE
 )
 group by FOOD_TYPE
 order by FOOD_TYPE desc;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/164668
+//조건에 맞는 사용자와 총 거래금액 조회하기
+SELECT u.USER_ID, u.NICKNAME, sum(b.PRICE) as TOTAL_SALES
+from USED_GOODS_BOARD b inner join USED_GOODS_USER u on b.WRITER_ID = u.USER_ID
+where b.STATUS = "DONE"
+group by USER_ID
+having TOTAL_SALES >=700000
+order by TOTAL_SALES;
