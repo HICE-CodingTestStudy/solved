36892751f286bf488594005ad7191d8444dca2cb
sjhlko <sjhlko@naver.com>
2023-07-11 20:02:54 +0900

    feat: sql 연습문제(string, date) 카테고리 별 상품 개수 구하기

--- sql/jeongHee/StringDate.java
+++ sql/jeongHee/StringDate.java
@@ -126,3 +126,10 @@ from ANIMAL_OUTS
 )
 order by datediff(b.DATETIME,a.DATETIME) desc
 limit 2;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/131529
+//카테고리 별 상품 개수 구하기
+SELECT substring(PRODUCT_CODE,1,2) as CATEGORY, count(*) as PRODUCTS
+from PRODUCT
+group by CATEGORY
+order by CATEGORY;
