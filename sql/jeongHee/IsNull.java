30394535e97310f1f34043c95c6d136659caaf48
sjhlko <sjhlko@naver.com>
2023-07-11 15:32:07 +0900

    feat: sql 연습문제(is null) 이름이 없는 동물의 아이디

--- sql/jeongHee/IsNull.java
+++ sql/jeongHee/IsNull.java
@@ -4,3 +4,10 @@ SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS, ifnull(FREEZER_YN,"N") as FREEZER_
 from FOOD_WAREHOUSE
 where ADDRESS like "경기%"
 order by WAREHOUSE_ID;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/59039
+//이름이 없는 동물의 아이디
+SELECT ANIMAL_ID
+from ANIMAL_INS
+where NAME is null
+order by ANIMAL_ID;
