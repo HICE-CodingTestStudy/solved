3b80435306e48d1b6de6c5fb223e2b255a9ee9ea
sjhlko <sjhlko@naver.com>
2023-07-11 19:06:07 +0900

    feat: sql 연습문제(string, date) 취소되지 않은 진료 예약 조회하기

--- sql/jeongHee/StringDate.java
+++ sql/jeongHee/StringDate.java
@@ -27,3 +27,11 @@ end as STATUS
 from USED_GOODS_BOARD
 where CREATED_DATE = "2022-10-05"
 order by BOARD_ID desc;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/132204
+//취소되지 않은 진료 예약 조회하기
+SELECT a.APNT_NO,p.PT_NAME,p.PT_NO,d.MCDP_CD,d.DR_NAME,a.APNT_YMD
+from APPOINTMENT a inner join PATIENT p on a.PT_NO = p.PT_NO
+inner join DOCTOR d on a.MDDR_ID = d.DR_ID
+where a.MCDP_CD = "CS" and a.APNT_YMD like "2022-04-13%" and a.APNT_CNCL_YMD is null
+order by a.APNT_YMD
