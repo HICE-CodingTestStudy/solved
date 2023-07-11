2ba3531a79d44b299bea107861aaa877d4d005aa
sjhlko <sjhlko@naver.com>
2023-07-11 15:32:56 +0900

    feat: sql 연습문제(is null) 이름이 있는 동물의 아이디

--- sql/jeongHee/IsNull.java
+++ sql/jeongHee/IsNull.java
@@ -11,3 +11,10 @@ SELECT ANIMAL_ID
 from ANIMAL_INS
 where NAME is null
 order by ANIMAL_ID;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/59407
+//이름이 있는 동물의 아이디
+SELECT ANIMAL_ID
+from ANIMAL_INS
+where NAME is not null
+order by ANIMAL_ID;
