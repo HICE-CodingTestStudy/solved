c9b8c53836db9d4bb44b96f2e54309d04f4ca796
sjhlko <sjhlko@naver.com>
2023-07-11 18:01:04 +0900

    feat: sql 연습문제(join) 상품 별 오프라인 매출 구하기

--- sql/jeongHee/Join.java
+++ sql/jeongHee/Join.java
@@ -76,3 +76,13 @@ SELECT i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
 from ANIMAL_INS i inner join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
 where i.SEX_UPON_INTAKE like "Intact%" and (o.SEX_UPON_OUTCOME like "Spayed%" or o.SEX_UPON_OUTCOME like "Neutered%")
 order by i.ANIMAL_ID
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/131533
+//상품 별 오프라인 매출 구하기
+SELECT a.PRODUCT_CODE , a.PRICE * b.AMOUNT as SALES
+from PRODUCT a inner join (
+select PRODUCT_ID, sum(SALES_AMOUNT) as AMOUNT
+from OFFLINE_SALE
+group by PRODUCT_ID
+) b on a.PRODUCT_ID = b.PRODUCT_ID
+order by SALES desc, a.PRODUCT_CODE ;
