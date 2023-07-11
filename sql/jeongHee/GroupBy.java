304bfb7caa2c12c7e9e00fe79a18cdde0d3c47f1
sjhlko <sjhlko@naver.com>
2023-07-11 13:33:34 +0900

    feat: sql 연습문제(group by) 입양 시각 구하기(1)

--- sql/jeongHee/GroupBy.java
+++ sql/jeongHee/GroupBy.java
@@ -120,3 +120,10 @@ group by NAME ) c
 where c.COUNT >=2 and NAME != ""
 order by c.NAME;
 
+//https://school.programmers.co.kr/learn/courses/30/lessons/59412
+//입양 시각 구하기(1)
+SELECT hour(DATETIME) as HOUR , count(*) as COUNT
+from ANIMAL_OUTS
+where hour(DATETIME) between 9 and 19
+group by hour(DATETIME)
+order by HOUR;
