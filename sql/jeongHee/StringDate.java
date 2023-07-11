135ccdc0fe8449555ab70292c94d170f9fb43b3a
sjhlko <sjhlko@naver.com>
2023-07-11 20:04:50 +0900

    feat: sql 연습문제(string, date) DATETIME에서 DATE로 형 변환

--- sql/jeongHee/StringDate.java
+++ sql/jeongHee/StringDate.java
@@ -133,3 +133,9 @@ SELECT substring(PRODUCT_CODE,1,2) as CATEGORY, count(*) as PRODUCTS
 from PRODUCT
 group by CATEGORY
 order by CATEGORY;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/59414
+//DATETIME에서 DATE로 형 변환
+SELECT ANIMAL_ID,NAME, date_format(DATETIME,"%Y-%m-%d") as 날짜
+from ANIMAL_INS
+order by ANIMAL_ID;
