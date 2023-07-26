c1a288c3e370a7952ee5f6bfdded2475f905ce9e
sjhlko <sjhlko@naver.com>
2023-07-26 10:11:01 +0900

    feat: 트리/최단거리 과제(상) 등산코스 정하기

--- disjointSet&MST/jeongHee/Mountain.java
+++ disjointSet&MST/jeongHee/Mountain.java
@@ -22,6 +22,10 @@ public class Mountain {
     * -> 시간 초과, 실패
     * */
 
+    static PriorityQueue<Node> pq = new PriorityQueue<>(
+            Comparator.comparingInt(o1->o1.weight)
+    );
+
     public static class Node{
         int index;
         int weight;
@@ -31,162 +35,73 @@ public class Mountain {
             this.weight = weight;
         }
     }
-    public static int dijkstra(int N, int source, int dest, List<Node>[] graph, boolean[] isTop, boolean[] isGate){
-        PriorityQueue<Node> pq = new PriorityQueue<>(
-                Comparator.comparingInt(o1->o1.weight)
-        );
-        int[] dist = new int[N+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[source] = 0;
-        pq.add(new Node(source,0));
+    public static int[] dijkstra(int[] dist, List<Node>[] graph, boolean[] isTop){
+        int min = Integer.MAX_VALUE;
+        int index = -1;
         while (!pq.isEmpty()){
             Node u = pq.poll();
-            for (Node v : graph[u.index]) {
-                if(dist[v.index]<=dist[u.index]) continue;
-                if(isTop[v.index]) continue;
+            if(isTop[u.index]) continue;
+            if(u.weight>=min) continue;
+            //꺼낸 점의 가중치 자체가 기존 min보다 크면 이걸 살펴볼 필요가 없음
+             for (Node v : graph[u.index]) {
+                if(dist[v.index]<=Math.max(dist[u.index],v.weight)) continue;
                 dist[v.index] = Math.max(dist[u.index],v.weight);
+                if(isTop[v.index]) {
+                    if(min<dist[v.index]) continue;
+                    if(min==dist[v.index]){
+                        index=Math.min(index, v.index);
+                        continue;
+                    }
+                    min = Math.min(min,dist[v.index]);
+                    index=v.index;
+                    continue;
+                }
                 pq.add(new Node(v.index, dist[v.index]));
+
             }
         }
-        return dist[dest];
-    }
-//    public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-//        List<Node>[] graph = new List[n+1];
-//        boolean[] isGate = new boolean[n+1];
-//        boolean[] isTop = new boolean[n+1];
-//        for (int gate : gates) {
-//            isGate[gate]=true;
-//        }
-//        for (int summit : summits) {
-//            isTop[summit]=true;
-//        }
-//        for (int i = 0; i < n+1; i++) {
-//            graph[i]=new ArrayList<>();
-//        }
-//        for (int[] path : paths) {
-//            graph[path[0]].add(new Node(path[1], path[2]));
-//            graph[path[1]].add(new Node(path[0], path[2]));
-//        }
-//        int top = -1;
-//        int intensity = Integer.MAX_VALUE;
-//        for (int summit : summits) {
-//            int ret = dijkstra(n,summit,graph,isTop,isGate);
-//            if(ret<intensity) {
-//                intensity = ret;
-//                top = summit;
-//            }
-//        }
-//        int[] ans = new int[2];
-//        ans[0]=top;
-//        ans[1]=intensity;
-//        return ans;
-//    }
-//
-
-    public static class Graph implements Comparable<Graph>{
-        int a;
-        int b;
-        int cost;
-
-        public Graph(int a, int b, int cost) {
-            this.a = a;
-            this.b = b;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Graph o) {
-            if(this.cost>o.cost) return 1;
-            if(this.cost<o.cost) return -1;
-            else return 0;
-        }
-    }
-    public static int find(int a, int[] graph){
-        if(graph[a]==-1) return a;
-        return graph[a]=find(graph[a],graph);
-    }
-    public static boolean union(int a, int b, int[] graph, int[] height){
-        int rootA = find(a,graph);
-        int rootB = find(b,graph);
-        if(rootB==rootA) return false;
-        if(rootA>=rootB) {
-            int tmp = rootA;
-            rootA = rootB;
-            rootB = tmp;
-        }
-        if(height[rootA]==height[rootB]){
-            graph[rootA]=rootB;
-            height[rootA]=-1;
-            height[rootB]++;
-            return true;
-        }
-        graph[rootA]=rootB;
-        height[rootA]=-1;
-        return true;
+        return new int[]{index,min};
     }
     public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        int[] graph = new int[n + 1];
-        int[] height = new int[n + 1];
-        boolean[] isGate = new boolean[n + 1];
-        boolean[] isTop = new boolean[n + 1];
-        int[] answer = new int[2];
-        int maxCost = -1;
-        for (int gate : gates) {
-            isGate[gate] = true;
+        List<Node>[] graph = new List[n+1];
+        boolean[] isGate = new boolean[n+1];
+        boolean[] isTop = new boolean[n+1];
+        int[] dist = new int[n+1];
+        for (int i = 0; i < n+1; i++) {
+            graph[i]=new ArrayList<>();
+            dist[i]=Integer.MAX_VALUE;
         }
-        for (int summit : summits) {
-            isTop[summit] = true;
-        }
-        Arrays.fill(graph, -1);
-        PriorityQueue<Graph> pq = new PriorityQueue<>();
-        for (int i = 0; i < paths.length; i++) {
-            pq.add(new Graph(paths[i][0], paths[i][1], paths[i][2]));
-        }
-        ArrayList<Integer> includedTop = new ArrayList<>();
-        ArrayList<Integer> includedStart = new ArrayList<>();
-        List<Node>[] listGraph = new List[n+1];
         for (int gate : gates) {
             isGate[gate]=true;
+            dist[gate]=0;
+            pq.add(new Node(gate,0));
         }
         for (int summit : summits) {
             isTop[summit]=true;
         }
-        for (int i = 0; i < n+1; i++) {
-            listGraph[i]=new ArrayList<>();
-        }
         for (int[] path : paths) {
-            listGraph[path[0]].add(new Node(path[1], path[2]));
-            listGraph[path[1]].add(new Node(path[0], path[2]));
-        }
-        while (!pq.isEmpty()) {
-            Graph g = pq.poll();
-            union(g.a, g.b, graph, height);
-            maxCost = Math.max(g.cost, maxCost);
-            if (isTop[g.a]) includedTop.add(g.a);
-            if (isTop[g.b]) includedTop.add(g.b);
-            if (isGate[g.a]) includedStart.add(g.a);
-            if (isGate[g.b]) includedStart.add(g.b);
-            if(pq.size()>=1&&pq.peek().cost==g.cost) continue;
-            if(!isTop[g.a]&&!isTop[g.b]&&!isGate[g.a]&&!isGate[g.b]) continue;
-            Collections.sort(includedTop);
-            for(int summit : includedTop) {
-                for (int gate : includedStart) {
-                    if (find(gate, graph) == find(summit, graph)) {
-                        if(dijkstra(n,summit,gate,listGraph,isTop,isGate)==maxCost) {
-                            answer[0] = summit;
-                            answer[1] = maxCost;
-                            return answer;
-                        }
-                    }
-                }
+            if(isTop[path[0]]||isGate[path[1]]){
+                graph[path[1]].add(new Node(path[0], path[2]));
+                continue;
             }
+            if(isTop[path[1]]||isGate[path[0]]){
+                graph[path[0]].add(new Node(path[1], path[2]));
+                continue;
+            }
+            graph[path[0]].add(new Node(path[1], path[2]));
+            graph[path[1]].add(new Node(path[0], path[2]));
         }
-
-        return answer;
+        int top = -1;
+        int intensity = Integer.MAX_VALUE;
+        int[] ret = dijkstra(dist,graph,isTop);
+        return ret;
     }
+
+
     public static void main(String[] args) {
-        solution(	7, new int[][]{{1, 4, 4}, {1, 6, 1}, {1, 7, 3}, {2, 5, 2}, {3, 7, 4}, {5, 6, 6}}, new int[]{1}, new int[]{2, 3, 4});
+       // solution(	7, new int[][]{{1, 4, 4}, {1, 6, 1}, {1, 7, 3}, {2, 5, 2}, {3, 7, 4}, {5, 6, 6}}, new int[]{1}, new int[]{2, 3, 4});
         //solution(	7, new int[][]{{1, 2, 5}, {1, 4, 1}, {2, 3, 1}, {2, 6, 7}, {4, 5, 1}, {5, 6, 1}, {6,7,1}}, new int[]{3,7}, new int[]{1,5});
+        solution(	6, new int[][]{{1, 2, 3}, {2, 3, 5}, {2, 4, 2}, {2, 5, 4}, {3, 4, 4}, {4, 5, 3},{4,6,1},{5,6,1}}, new int[]{1,3}, new int[]{5});
 
     }
 }
