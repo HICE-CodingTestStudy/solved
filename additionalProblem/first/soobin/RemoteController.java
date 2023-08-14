347290cce6e4697e039a64b0d1662c8d8d4705f8
s0o0bn <0921soobin@gmail.com>
2023-08-14 17:07:34 +0900

    FIX 추가 문제 디렉토리 명 변경

---
+++ additionalProblem/first/soobin/RemoteController.java
@@ -0,0 +1,66 @@
+package first.soobin;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class RemoteController {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final int START_CHANNEL = 100;
+
+    private static final Set<Character> broken = new HashSet<>();
+
+    private static int solution(int N, int M) {
+        if (N == 100) return 0;
+        if (M == 10) return N > START_CHANNEL ? N - START_CHANNEL : START_CHANNEL - N;
+
+        int currentMin = Integer.MAX_VALUE;
+        if (N < 100) {
+            for (int i = 100; i >= 0; i--) {
+                String channel = String.valueOf(i);
+                boolean canPushButton = true;
+
+                for (char button : channel.toCharArray()) {
+                    if (broken.contains(button)) {
+                        canPushButton = false;
+                        break;
+                    }
+                }
+                if (!canPushButton) continue;
+
+                currentMin = Math.min(Math.min(Math.abs(N - i) + channel.length(), START_CHANNEL - N), currentMin);
+            }
+            return Math.min(currentMin, START_CHANNEL - N);
+        }
+
+        for (int i = START_CHANNEL; i - N <= N - START_CHANNEL; i++) {
+            String channel = String.valueOf(i);
+            boolean canPushButton = true;
+
+            for (char button : channel.toCharArray()) {
+                if (broken.contains(button)) {
+                    canPushButton = false;
+                    break;
+                }
+            }
+            if (!canPushButton) continue;
+
+            currentMin = Math.min(Math.min((Math.abs(N - i)) + channel.length(), N - START_CHANNEL), currentMin);
+        }
+        return Math.min(currentMin, N - START_CHANNEL);
+    }
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        if (M > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) broken.add(st.nextToken().charAt(0));
+        }
+
+        bw.write(String.valueOf(solution(N, M)));
+        bw.flush();
+    }
+}
---
+++ additionalProblem/second/soobin/SmashingWall.java
@@ -0,0 +1,72 @@
+package second.soobin;
+
+import java.io.*;
+import java.util.*;
+
+public class SmashingWall {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final int[][] moves = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+    private static char[][] map;
+    private static List<int[]> walls;
+    private static int N, M;
+
+    private static void bfs(int[] start, int[][] visited) {
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.add(start);
+        visited[start[0]][start[1]] = 1;
+
+        while (!queue.isEmpty()) {
+            int[] n = queue.poll();
+
+            for (int[] move : moves) {
+                int nr = n[0] + move[0], nc = n[1] + move[1];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc] != 0) continue;
+
+                if (map[nr][nc] == '1') {
+                    visited[nr][nc] = visited[n[0]][n[1]] + 1;
+                    continue;
+                }
+
+                queue.add(new int[] {nr, nc});
+                visited[nr][nc] = visited[n[0]][n[1]] + 1;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
+        walls = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < M; j++) {
+                char type = row.charAt(j);
+                if (type == '1') walls.add(new int[] {i, j});
+                map[i][j] = type;
+            }
+        }
+
+        int[][] start = new int[N][M];
+        int[][] end = new int[N][M];
+        bfs(new int[] {0, 0}, start);
+        bfs(new int[] {N - 1, M - 1}, end);
+
+        int min = Integer.MAX_VALUE;
+        for (int[] wall : walls) {
+            int wr = wall[0], wc = wall[1];
+            if (start[wr][wc] != 0 && end[wr][wc] != 0)
+                min = Math.min(start[wr][wc] + end[wr][wc] - 1, min);
+        }
+
+        if (walls.size() == 0) min = end[0][0];
+
+        bw.write(String.valueOf(min == Integer.MAX_VALUE ? -1 : min));
+        bw.flush();
+    }
+}
