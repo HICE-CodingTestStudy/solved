6ccdfdc721458070489ce526d324b86f09c6fb56
s0o0bn <0921soobin@gmail.com>
2023-08-21 16:54:54 +0900

    FEAT 프로그래머스 49993 스킬트리 성공

---
+++ additionalProblem/fifth/soobin/SkillTree.java
@@ -0,0 +1,48 @@
+package fifth.soobin;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class SkillTree {
+    private boolean isAvailable(String skill, String skillTree) {
+        Set<Character> precedingSet = new HashSet<>();
+        Queue<Character> skillLevel = new LinkedList<>();
+        for (char level : skill.toCharArray()) {
+            skillLevel.add(level);
+            precedingSet.add(level);
+        }
+
+        boolean[] needPreceding = new boolean[skillTree.length()];
+        for (int i = 0; i < skillTree.length(); i++)
+            needPreceding[i] = precedingSet.contains(skillTree.charAt(i));
+
+        for (int i = 0; i < skillTree.length(); i++) {
+            char currentSkill = skillTree.charAt(i);
+
+            if (!needPreceding[i]) continue;
+
+            char level = skillLevel.peek();
+            if (currentSkill == level) skillLevel.poll();
+            else return false;
+        }
+
+        return true;
+    }
+
+    public int solution(String skill, String[] skillTrees) {
+        int answer = 0;
+        for (String tree: skillTrees)
+            if (isAvailable(skill, tree))
+                answer++;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SkillTree s = new SkillTree();
+
+        System.out.println(s.solution("CBD", new String[] {"BACDE", "CBADF", "AECB", "BDA"}));
+    }
+}
