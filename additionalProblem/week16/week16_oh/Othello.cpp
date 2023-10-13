e59238b35465ed842d2baef7d70b01c3ca979572
ohsopp <wjdgus0545@naver.com>
2023-10-13 14:59:39 +0900

    Update Othello.cpp

--- additionalProblem/week16/week16_oh/Othello.cpp
+++ additionalProblem/week16/week16_oh/Othello.cpp
@@ -1,40 +1,33 @@
 
 #include <iostream>
-
 using namespace std;
 
 // empty:0	Black:1	White:2
 int n, R, C, board[8][8];
-
+char m[3] = { '.', 'B', 'W' };
 int dx[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 int dy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 bool flag;
 
-void convert(int r, int c, int dir, int color) {
-
-	if (board[r][c] == color || !r || !c) {
+void convert(int r, int c, int dir) {
+	if (board[r][c] == board[R][C] || !r || !c) {
 		flag = 1;
 		return;
 	}
 
 	int nr = r + dx[dir], nc = c + dy[dir];
 	if (board[nr][nc]) {
-		convert(nr, nc, dir, color);
-		if (flag) board[r][c] = color;
+		convert(nr, nc, dir);
+		if (flag) board[r][c] = board[R][C];
 	}
 }
 
 int printMap() {
-	
 	int res = 0;
 	for (int i = 1; i <= 6; i++) {
 		for (int j = 1; j <= 6; j++) {
-			char c = ' ';
-			if (!board[i][j]) c = '.';
-			else if (board[i][j] == 1) c = 'B', res++;
-			else c = 'W', res--;
-
-			cout << c;
+			res = res + board[i][j] % 2 - board[i][j] / 2;
+			cout << m[board[i][j]];
 		}
 		cout << endl;
 	}
@@ -57,12 +50,10 @@ int main() {
 			int nr = R + dx[i], nc = C + dy[i];
 			if (board[nr][nc] && board[nr][nc] != board[R][C]) {
 				flag = 0;
-				convert(nr, nc, i, board[R][C]);
+				convert(nr, nc, i);
 			}
 		}
-
 		bTurn = !bTurn;
 	}
-
 	puts(printMap() > 0 ? "Black" : "White");
 }
