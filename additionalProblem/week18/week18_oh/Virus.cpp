3564da790bfa46ce1b4da2b87cf3c257932d9a4e
ohsopp <wjdgus0545@naver.com>
2023-10-20 23:12:11 +0900

    Update Virus.cpp

--- additionalProblem/week18/week18_oh/Virus.cpp
+++ additionalProblem/week18/week18_oh/Virus.cpp
@@ -10,7 +10,6 @@ bool visited[202][202];
 
 int main() {
 	cin >> n >> k;
-
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++) {
 			cin >> board[i][j];
@@ -18,19 +17,14 @@ int main() {
 		}
 
 	cin >> s >> x >> y;
-
 	while (s--) {
 		for (int v = 1; v <= k; v++) {
 			int v_size = virus[v].size();
-
 			for (int i = 0; i < v_size; i++) {
 				int r = virus[v][i].first, c = virus[v][i].second;
-
 				if (visited[r][c]) continue;
-
 				for (int j = 0; j < 4; j++) {
 					int nr = r + dx[j], nc = c + dy[j];
-
 					if (nr && nc && nr <= n && nc <= n && !board[nr][nc]) {
 						board[nr][nc] = v;
 						virus[v].push_back({ nr, nc });
