b5740f41f070b85e9f7675ee6e52d3f5e29e93a2
ohsopp <wjdgus0545@naver.com>
2023-10-20 23:13:07 +0900

    Update LevelHamburger.cpp

--- additionalProblem/week18/week18_oh/LevelHamburger.cpp
+++ additionalProblem/week18/week18_oh/LevelHamburger.cpp
@@ -12,31 +12,25 @@ void f(ll lv, ll len) {
 		ans++;
 		return;
 	}
-
-	len--;	// 가장 왼쪽 햄버거번 1개 빼주기
+	len--;	// 가장 왼쪽 햄버거번 -1
 	if (len == b[lv - 1]) {
 		ans += p[lv - 1];
 		f(lv - 1, len - b[lv - 1]);
 	}
-
 	else if (len < b[lv - 1])
 		f(lv - 1, len);
-
 	else {
 		ans += p[lv - 1] + 1;
 		f(lv - 1, len - b[lv - 1] - 1);	// 가운데 패티 1개 빼주기
 	}
-
 }
 
 int main() {
 	cin >> N >> X;
-
 	for (int i = 1; i <= N; i++) {
 		b[i] = b[i - 1] * 2 + 3;
 		p[i] = p[i - 1] * 2 + 1;
 	}
-
 	f(N, X);
     cout << ans;
 }
