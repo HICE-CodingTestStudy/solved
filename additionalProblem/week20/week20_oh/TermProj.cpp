eb42bcc0a8abde979914f938d2b56c4d2972f550
ohsopp <wjdgus0545@naver.com>
2023-10-27 22:05:58 +0900

    Update TermProj.cpp

--- additionalProblem/week20/week20_oh/TermProj.cpp
+++ additionalProblem/week20/week20_oh/TermProj.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int t, n, ans, a[100001], cnt[100001];
-queue<int> q;
+queue<int> q;	// 팀에 못 들어가는 사람
 
 int main() {
 	cin >> t;
@@ -16,7 +16,7 @@ int main() {
 		cin >> n;
 		for (int i = 1; i <= n; i++) {
 			cin >> a[i];
-			cnt[a[i]]++;
+			cnt[a[i]]++;	// 선택된 횟수
 		}
 
 		for (int i = 1; i <= n; ++i)
@@ -26,7 +26,7 @@ int main() {
 			int cur = q.front();
 			q.pop();
 			ans++;
-			if (--cnt[a[cur]] == 0) q.push(a[cur]);
+			if (--cnt[a[cur]] == 0) q.push(a[cur]);	// <선택받지 못한 사람>의 픽이 선택된 횟수가 1이면 팀에 못 들어가므로 q에 추가
 		}
 		cout << ans << endl;
 	}
