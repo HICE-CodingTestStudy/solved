28d143275b329994da4aafaad101eeb0447c2e9d
sjhlko <sjhlko@naver.com>
2023-11-01 07:08:23 +0900

    feat: 추가 문제 풀이 21회차 (상) 너 봄에는 캡사이신이 맛있단다

--- additionalProblem/week21/week21_jeongHee/JeomSooni.java
+++ additionalProblem/week21/week21_jeongHee/JeomSooni.java
@@ -3,6 +3,7 @@ package additional2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
 
 public class JeomSooni {
@@ -11,17 +12,29 @@ public class JeomSooni {
     static int N;
     static long division = 1000000007;
     static List<Long> scoville = new ArrayList<>();
-    static long ans = 0;
+    static BigInteger ans = BigInteger.ZERO;
     static long[] combinationCount;
 
-    static void solution(int left, int right) {
-        ans += ((scoville.get(right) - scoville.get(left)) * (combinationCount[right - left - 1] + 1)) % division;
+//    //(최대,최소) 쌍을 모두 찾는다 -> 30만 제한은 불가한 풀이, 3천 제한은 가능
+//    //정렬한 스코빌 지수 리스트에서 해당 최소 인덱스, 최대 인덱스를 사이에 둔 메뉴의 개수에 따라
+//    //해당 최대, 최소 쌍에 대한 메뉴 조합이 정해진다.
+//    //n개로 만들 수 있는 1개 메뉴 조합, 2개 메뉴 조합 + ... n개 메뉴 조합의 총 수 = (n-1)조합개수 * 2 + 1
+//    static void solution(int left, int right) {
+//        ans += ((scoville.get(right) - scoville.get(left)) * (combinationCount[right - left - 1] + 1)) % division;
+//    }
+
+
+    //쌍을 굳이 만들지 않고 n이최대값이 될 수 있는 경우의 수, 최솟값이 될 수 있는 경우의 수에 대한 계산을 한다.
+    static void solution(int index) {
+        if(index!=0){
+            ans=ans.add(BigInteger.valueOf(combinationCount[index]*scoville.get(index))).add(BigInteger.valueOf(division));
+        }
+        if(index!=N-1){
+            ans=ans.subtract(BigInteger.valueOf(combinationCount[N-index-1]*scoville.get(index))).mod(BigInteger.valueOf(division));
+        }
+        ans=ans.mod(BigInteger.valueOf(division));
     }
 
-    //(최대,최소) 쌍을 모두 찾는다 -> 30만 제한은 불가한 풀이, 3천 제한은 가능
-    //정렬한 스코빌 지수 리스트에서 해당 최소 인덱스, 최대 인덱스를 사이에 둔 메뉴의 개수에 따라
-    //해당 최대, 최소 쌍에 대한 메뉴 조합이 정해진다.
-    //n개로 만들 수 있는 1개 메뉴 조합, 2개 메뉴 조합 + ... n개 메뉴 조합의 총 수 = (n-1)조합개수 * 2 + 1
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine());
@@ -35,11 +48,17 @@ public class JeomSooni {
             scoville.add(Long.parseLong(st.nextToken()));
         }
         Collections.sort(scoville);
+
+        //50점 풀이
+//        for (int i = 0; i < N; i++) {
+//            for (int j = i + 1; j < N; j++) {
+//                solution(i, j);
+//            }
+//        }
+
         for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                solution(i, j);
-            }
+            solution(i);
         }
-        System.out.println(ans % division);
+        System.out.println(ans.mod(BigInteger.valueOf(division)));
     }
 }
