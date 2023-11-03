bdcacfb7f977598ee4b3c7aa0125d18086b345ba
ohsopp <wjdgus0545@naver.com>
2023-11-04 01:05:43 +0900

    Update ascii.cpp

--- additionalProblem/week22/week22_oh/ascii.cpp
+++ additionalProblem/week22/week22_oh/ascii.cpp
@@ -7,14 +7,14 @@ char c;
 double ans;
 
 int main() {
-	cin >> h >> w;
+    cin >> h >> w;
     while (h--) {
         W = w;
         while (W--) {
             cin >> c;
-			if (c == '.') ans = flag ? ans + 1 : ans;
-			else ans += 0.5, flag = !flag;
+            if (c == '.') ans = flag ? ans + 1 : ans;
+            else ans += 0.5, flag = !flag;
         }
     }
-	cout << ans;
+    cout << ans;
 }
