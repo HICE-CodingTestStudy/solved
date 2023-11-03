74facb2e2cdc5a195017117e4d30fd2e0b9956d1
ohsopp <wjdgus0545@naver.com>
2023-11-04 00:56:35 +0900

    week22

---
+++ additionalProblem/week22/week22_oh/ascii.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int h, w, flag;
+char c;
+double ans;
+
+int main() {
+	cin >> h >> w;
+
+	for (int i = 0; i < h; i++)
+		for (int j = 0; j < w; j++) {
+			cin >> c;
+			if (c == '.')
+                ans = flag ? ans + 1 : ans;
+			else {
+				ans += 0.5;
+                flag = !flag;
+			}
+		}
+	cout << ans;
+}
