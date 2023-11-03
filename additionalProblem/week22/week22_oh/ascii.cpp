117bcf335cdf7dd2b9a8ed6a8de051490634e74e
ohsopp <wjdgus0545@naver.com>
2023-11-04 00:59:22 +0900

    Update ascii.cpp

--- additionalProblem/week22/week22_oh/ascii.cpp
+++ additionalProblem/week22/week22_oh/ascii.cpp
@@ -7,17 +7,17 @@ char c;
 double ans;
 
 int main() {
-	cin >> h >> w;
-
-	for (int i = 0; i < h; i++)
-		for (int j = 0; j < w; j++) {
-			cin >> c;
-			if (c == '.')
+    cin >> h >> w;
+    
+    for (int i = 0; i < h; i++)
+        for (int j = 0; j < w; j++) {
+            cin >> c;
+            if (c == '.')
                 ans = flag ? ans + 1 : ans;
-			else {
-				ans += 0.5;
+            else {
+                ans += 0.5;
                 flag = !flag;
-			}
-		}
-	cout << ans;
+            }
+        }
+    cout << ans;
 }
