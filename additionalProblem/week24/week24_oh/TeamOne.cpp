f0b8f22b2ece47355a7141b0c1e06fdbaa7c86af
ohsopp <wjdgus0545@naver.com>
2023-11-11 08:53:33 +0900

    Create TeamOne.cpp

---
+++ additionalProblem/week24/week24_oh/TeamOne.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m, o, p, ans = 15;
+vector<int> team, v[11];
+
+void f(int idx, int cnt) {
+	
+	if (cnt) {
+		bool solved[11] = { false };
+		for (int i = 0; i < cnt; i++)
+			for (int j = 0; j < v[team[i]].size(); j++)
+				solved[v[team[i]][j]] = 1;
+			
+		bool flag = true;
+		for (int i = 1; i <= n; i++) 
+			if (!solved[i]) {
+				flag = false;
+				break;
+			}
+		if (flag) ans = min(ans, cnt);
+	}
+
+	for (int i = idx; i <= m; i++) {
+		team.push_back(i);
+		f(i + 1, cnt + 1);
+		team.pop_back();
+	}
+}
+
+int main() {
+	cin >> n >> m;
+  
+	for (int i = 1; i <= m; i++) {
+		cin >> o;
+		for (int j = 0; j < o; j++) {
+			cin >> p;
+			v[i].push_back(p);
+		}
+	}
+
+	f(1, 0);
+
+	ans > 10 ? cout << -1 : cout << ans;
+}
