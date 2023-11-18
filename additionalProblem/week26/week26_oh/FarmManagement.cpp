e559e28d0d5e1fe32b15894e3c93ca4cabad7c9a
ohsopp <wjdgus0545@naver.com>
2023-11-18 12:54:49 +0900

    Create FarmManagement.cpp

---
+++ additionalProblem/week26/week26_oh/FarmManagement.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+int n, m, ans, map[102][72];
+int dx[] = { -1, -1, 0, 1, 1, 1, 0, -1 }, dy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+bool v[102][72];
+
+int dfs(int x, int y, int t) {
+	if (map[x][y] != t) return map[x][y] < t;
+	if (v[x][y]) return 1;
+	v[x][y] = 1;
+
+	int ret = 1;
+	for (int i = 0; i < 8; i++) {
+		int nx = x + dx[i], ny = y + dy[i];
+		ret &= dfs(nx, ny, t);
+	}
+	return ret;
+}
+
+int main() {
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			cin >> map[i][j];
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			if (!v[i][j] && map[i][j])
+				ans += dfs(i, j, map[i][j]);
+	
+	cout << ans;
+}
