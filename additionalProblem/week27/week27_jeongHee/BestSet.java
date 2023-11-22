7a9439a9597f9b9a60fc5d0263868007dc0fd316
sjhlko <sjhlko@naver.com>
2023-11-22 10:33:08 +0900

    feat: 추가 문제 풀이 27회차 과제(상) 최고의 집합 수정

--- additionalProblem/week27/week27_jeongHee/BestSet.java
+++ additionalProblem/week27/week27_jeongHee/BestSet.java
@@ -4,10 +4,10 @@ public class BestSet {
     //https://school.programmers.co.kr/learn/courses/30/lessons/12938
     //최고의 집합
     public int[] solution(int n, int s) {
-        int[] ans = new int[n];
         if (s / n == 0) {
             return new int[]{-1};
         }
+        int[] ans = new int[n];
         for (int i = 0; i < n - s % n; i++) {
             ans[i] = (s / n);
         }
