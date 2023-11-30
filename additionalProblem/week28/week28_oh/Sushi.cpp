648404145c33f20266fb4c9cd3f671773ee5a884
ohsopp <wjdgus0545@naver.com>
2023-11-30 12:23:05 +0900

    Update Sushi.cpp

--- additionalProblem/week28/week28_oh/Sushi.cpp
+++ additionalProblem/week28/week28_oh/Sushi.cpp
@@ -2,24 +2,24 @@
 #include <iostream>
 using namespace std;
 
-int n, d, k, c, num, ans, a, ary[30001], check[3001];
+int n, d, k, c, num, ans, a, ary[30001], cnt[3001];
 
 int main() {
 	cin >> n >> d >> k >> c;
-	check[c]++;
+	cnt[c]++;
 	a++;
 
 	for (int i = 0; i < n; i++)
 		cin >> ary[i];
   
 	for (int i = 0; i < k; i++)
-   		 a += !check[ary[i]]++;
+   		 a += !cnt[ary[i]]++;
   
 	ans = a;
   
 	for (int i = 1; i < n; i++) {
-		a -= !(--check[ary[i - 1]]);
-		a += !check[ary[(i + k - 1) % n]]++;
+		a -= !(--cnt[ary[i - 1]]);
+		a += !cnt[ary[(i + k - 1) % n]]++;
 		ans = max(ans, a);
 	}
 	cout << ans;
