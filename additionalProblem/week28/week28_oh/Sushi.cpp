b6cbafb1f8fc7e97881ac75ba8e461c539978995
ohsopp <wjdgus0545@naver.com>
2023-11-30 12:20:41 +0900

    Create Sushi.cpp

---
+++ additionalProblem/week28/week28_oh/Sushi.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int n, d, k, c, num, ans, a, ary[30001], check[3001];
+
+int main() {
+	cin >> n >> d >> k >> c;
+	check[c]++;
+	a++;
+
+	for (int i = 0; i < n; i++)
+		cin >> ary[i];
+  
+	for (int i = 0; i < k; i++)
+    a += !check[ary[i]]++;
+  
+	ans = a;
+  
+	for (int i = 1; i < n; i++) {
+		a -= !(--check[ary[i - 1]]);
+		a += !check[ary[(i + k - 1) % n]]++;
+		ans = max(ans, a);
+	}
+	cout << ans;
+}
