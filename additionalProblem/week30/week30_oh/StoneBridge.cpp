d16a155e089bb7defc5536fdabc53dbee28bcea6
ohsopp <wjdgus0545@naver.com>
2023-12-09 17:41:43 +0900

    Update StoneBridge.cpp

--- additionalProblem/week30/week30_oh/StoneBridge.cpp
+++ additionalProblem/week30/week30_oh/StoneBridge.cpp
@@ -6,8 +6,8 @@ int dp[2][101], k=0;
 string s, b[2];
 
 int main() {
-  cin >> s >> b[0] >> b[1];
-  dp[0][0] = dp[1][0] = 1;
+	cin >> s >> b[0] >> b[1];
+	dp[0][0] = dp[1][0] = 1;
 
 	for (int i = 0; i < b[0].size(); i++)
 		for (int j = s.size() - 1; j >= 0; j--) {
@@ -15,5 +15,5 @@ int main() {
 			if (s[j] == b[1][i]) dp[0][j + 1] += dp[1][j];
 		}
   
-    cout << dp[0][s.size()] + dp[1][s.size()];
+	cout << dp[0][s.size()] + dp[1][s.size()];
 }
