ea9dd938b88f2da16f95bf8441ae8e3263ea337c
ohsopp <wjdgus0545@naver.com>
2024-01-14 19:44:42 +0900

    Update NumCard.cpp

--- additionalProblem/week34/week34_oh/NumCard.cpp
+++ additionalProblem/week34/week34_oh/NumCard.cpp
@@ -9,10 +9,10 @@ int main() {
 	cin >> s;
 	int l = s.size();
 	s = " " + s;
-  s[1] -= '0';
+	s[1] -= '0';
   
 	for (int i = 2; i <= l; i++) {
-    s[i] -= '0';
+		s[i] -= '0';
 		int num = s[i - 1] * 10 + s[i];
 		if (s[i]) dp[i] += dp[i - 1];
 		if (num < 35 && s[i - 1]) dp[i] += dp[i - 2];
