4cb57698f91d95f6067a3016e9bfc68c646baf8d
ohsopp <wjdgus0545@naver.com>
2024-01-14 19:44:26 +0900

    Create NumCard.cpp

---
+++ additionalProblem/week34/week34_oh/NumCard.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+string s;
+long long dp[41] = { 1, 1 };
+
+int main() {
+	cin >> s;
+	int l = s.size();
+	s = " " + s;
+  s[1] -= '0';
+  
+	for (int i = 2; i <= l; i++) {
+    s[i] -= '0';
+		int num = s[i - 1] * 10 + s[i];
+		if (s[i]) dp[i] += dp[i - 1];
+		if (num < 35 && s[i - 1]) dp[i] += dp[i - 2];
+	}
+
+	cout << dp[l];
+}
