f4ebe0c25b97535121ff4a6a4ec991a7d031c1c5
EraMorgett4 <78586125+EraMorgett4@users.noreply.github.com>
2024-02-20 21:18:34 +0900

    Add files via upload

---
+++ additionalProblem/week37_hsw/BOJ 14225 부분수열의 합.py
@@ -0,0 +1,14 @@
+#BOJ 14225 부분수열의 합
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = list(map(int, input().split()))
+s.sort()
+
+sum = 0
+for i in s
+    if sum + 1  i
+        break
+    sum += i
+print(sum + 1)
