86a15557f59d884d5436a43f73f56929a1980134
sjhlko <sjhlko@naver.com>
2024-10-02 23:18:45 +0900

    fix : 추가 문제 풀이 57회차 돌 그룹 수정

--- additionalProblem/week57/jeongHee/Stone.java
+++ additionalProblem/week57/jeongHee/Stone.java
@@ -9,22 +9,17 @@ public class Stone {
     // https://www.acmicpc.net/problem/12886
     // 돌 그룹
     static int[][] move = {{0, 1}, {0, 2}, {1, 2}};
-    static Set<String> visited = new HashSet<>();
+    static Set<String> visited;
 
     static boolean solution(int[] info) {
+        if(info[0]==info[1] && info[1]==info[2]) return true;
         for (int i = 0; i < 3; i++) {
             int l = move[i][0];
             int r = move[i][1];
             if (info[l] == info[r]) continue;
             int[] tmp = info.clone();
-            if (info[l] > info[r]) {
-                tmp[l] -= info[r];
-                tmp[r] += info[r];
-            }
-            else {
-                tmp[r] -= info[l];
-                tmp[l] += info[l];
-            }
+            tmp[r] -= info[l];
+            tmp[l] += info[l];
             Arrays.sort(tmp);
             String next = tmp[0] + " " + tmp[1] + " " + tmp[2];
             if(visited.contains(next)) continue;
@@ -42,6 +37,7 @@ public class Stone {
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int[] first = new int[]{a, b, c};
+        visited = new HashSet<>();
         Arrays.sort(first);
         visited.add(first[0] + " " + first[1] + " " + first[2]);
         boolean ans = solution(first);
